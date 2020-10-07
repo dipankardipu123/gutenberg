298a7d179ebf8e401a829dbdfa68892fce55b919
Noah Allen <noahtallen@gmail.com>
2020-10-07 11:56:14 -0700

    Refactor template info calculations

--- packages/edit-site/src/components/header/document-actions/index.js
+++ packages/edit-site/src/components/header/document-actions/index.js
@@ -21,6 +21,7 @@ import { useRef } from '@wordpress/element';
  * Internal dependencies
  */
 import TemplateDetails from '../../template-details';
+import { getTemplateInfo } from '../../../utils';
 
 function getBlockDisplayText( block ) {
 	return block
@@ -80,7 +81,7 @@ function useSecondaryText() {
 }
 
 export default function DocumentActions( { template } ) {
-	const documentTitle = template?.slug;
+	const { title: documentTitle } = getTemplateInfo( template );
 	const { label, isActive } = useSecondaryText();
 
 	// Title is active when there is no secondary item, or when the secondary
